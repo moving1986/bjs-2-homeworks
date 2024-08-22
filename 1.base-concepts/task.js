4b54700c5851c258a0d63e9887d662838e0fc89b
Aleksei Mezentsev <aleksei_mezencev@mail.ru>
2024-08-22 14:02:56 +0700

    Finished the first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -12,6 +12,15 @@ function solveEquation(a, b, c) {
     arr = -b/(2*a);
   }
 
+  if(arr > 0) {
+
+   let root1 = (-b + Math.sqrt(d) )/(2*a);
+   let root2 = (-b - Math.sqrt(d) )/(2*a);
+
+   arr = [root1, root2];
+
+  }
+
   return arr;
 }
 
