5aa4cb3bd436dfac93907f4118b8da0318fdd54a
Aleksei Mezentsev <aleksei_mezencev@mail.ru>
2024-08-22 13:54:48 +0700

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,6 +2,16 @@
 function solveEquation(a, b, c) {
   let arr = [];
   
+  arr = Math.pow(b, 2) - 4 * a * c;
+
+  if(arr < 0) {
+     arr = [];
+  }
+
+  if(arr === 0) {
+    arr = -b/(2*a);
+  }
+
   return arr;
 }
 
