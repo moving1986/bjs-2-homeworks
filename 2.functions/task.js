e8347479c7df0a64833be35bbe3265e3b0248fcd
Alexey Mezentsev <aleksei_mezencev@mail.ru>
2024-08-29 15:52:35 +0700

    Initial commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,24 @@
 function getArrayParams(...arr) {
 
+  let min = 2;
+  let max = 10;
+  let sum = min + max;
+  let num
+
+  for(let i = 0; i < length.arr; i++) {
+    
+    if(arr[i] > max) {
+      max = arr[i];
+    }
+
+    if(arr[i] < min) {
+      min = arr[i];
+    }
+
+     += sum;
+
+  }
+  console.log({min: min, max: max, avg: avg})
   return { min: min, max: max, avg: avg };
 }
 
