24992442d11449a2735717e5a20ff5368df48fca
Aleksey Mezentsev <aleksei_mezenccev@mail.ru>
2024-09-27 21:00:10 +0700

    All tasks complite

---
+++ task.js
@@ -0,0 +1,41 @@
+function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+}
+
+let alex = new Student("Алексей", "Мужской", 38);
+let vasya = new Student("Василий", "Мужской", 22);
+
+Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+
+}
+
+Student.prototype.addMarks = function (...marks) {
+    if (this.marks) {
+        this.marks.push(...marks);
+    } else {
+        console.log("У вас нет оценок");
+    }
+}
+
+Student.prototype.getAverage = function () {
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+    } else {
+        let sum = 0;
+        for (let i = 0; i < this.marks.length; i++) {
+            sum += this.marks[i];
+        }
+        return sum / this.marks.length
+    }
+
+}
+
+Student.prototype.exclude = function (reason) {
+    this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
+}
